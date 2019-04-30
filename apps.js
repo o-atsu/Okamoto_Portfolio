dd761e5d390b2eb432b7364811dacd28dbcbb53e
o-atsu <39590619+o-atsu@users.noreply.github.com>
2019-05-01 01:49:36 +0900

    0501_!
    紹介画像用意
    ※読み込み不可

--- apps.js
+++ apps.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', init);
 function init(){
-	if(!Detector.webgl) Detector.addGetWebGLMessage();
 	const width = window.innerWidth - 10;
 	const height = window.innerHeight - 300;
 	const renderer = new THREE.WebGLRenderer({
@@ -11,17 +10,16 @@ function init(){
 	renderer.setSize(width, height);
 	const scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(45, width / height);
-	camera.position.set(0, 0, +1000);
+	camera.position.set(0, 0, 800);
 
 	//ライト
 	const light = new THREE.AmbientLight(0xffffff, 1.0);
-	light.position.set(1, 1, 1,);
 	scene.add(light);
 	
 	//レイキャスタ
 	var projector = new THREE.Projector();
 	var mouse = {x:0, y:0};
-	canvas.addEventListener('mousemove', pointing, false);
+	window.addEventListener('mousemove', pointing, false);
 	function pointing(e){
 		const element = e.currentTarget;
 		mouse.x = ((e.clientX - e.offsetLeft) / width)*2-1;
@@ -33,19 +31,38 @@ function init(){
 	
 	//各紹介ボックス
 	var loader = new THREE.TextureLoader();
-	loader.load('', function(texture){});
+	loader.load('textures/BunboguFight!!!_tex.png', function(texture){
+		var bunboguFight = new THREE.Mesh(
+			new THREE.BoxGeometry(300, 300, 300),
+			new THREE.MeshBasicMaterial({map: texture}));
+		bunboguFight.position.set(100, 200, 100);
+		scene.add(bunboguFight);
+	});
 	
+	loader.load('textures/gassaku_tex.png', function(texture){
+		var gassaku = new THREE.Mesh(
+			new THREE.BoxGeometry(300, 300, 300),
+			new THREE.MeshBasicMaterial({map: texture}));
+		gassaku.position.set(-100, -100, 100);
+		scene.add(gassaku);
+	});
+
 	tick();
 	function tick(){
 		//レイキャスト
 		raycaster.setFromCamera(mouse, camera);
-		const intersects = raycaster.intersectObjects(scene.children);
-		if(intersects.length > 0){
+		var intersects = raycaster.intersectObjects(scene.children);
+		if(intersects.length > 0 && mesh === intersects[0].object){
 			//ぶつかったものはintersects内
+			if(intersects[0].object.name == bunboguFight){
+				window.addEventListener('mousedown', function(e){
+					window.location.href = 'https://github.com/o-atsu/Bunbogu_Fight';
+				}, false);
 		}
 
 		renderer.render(scene, camera);
 		requestAnimationFrame(tick);
 	}
 
+}
 }
